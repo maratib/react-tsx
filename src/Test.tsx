31c4f09956e745dfa8abd96620d7fbfd5869a6ef
Maratib <maratib@gmail.com>
2021-02-05 06:33:38 +0500

    intial commit

---
+++ src/Test.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export interface ITestProps {
+}
+
+export interface ITestState {
+}
+
+export default class Test extends React.Component<ITestProps, ITestState> {
+  constructor(props: ITestProps) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ src/components/Hello.tsx
@@ -0,0 +1,8 @@
+type HelloProps = {};
+
+const Hello = (props: HelloProps) => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
+export default Hello;
